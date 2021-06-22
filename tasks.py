b23b92975329c93058c56300de0804b1e8332830
BenedictusAryo <benedictusaryo@outlook.com>
2021-06-22 07:46:08 +0700

    update task

--- tasks.py
+++ tasks.py
@@ -2,7 +2,9 @@ from celery import Celery
 from time import sleep
 
 
-app = Celery('tasks', broker='localhost')
+# app = Celery('tasks', broker='localhost')
+app = Celery('tasks', broker='amqp://django:django@192.168.0.117')
+# app = Celery('tasks', broker='localhost')
 
 @app.task
 def reverse(text):
